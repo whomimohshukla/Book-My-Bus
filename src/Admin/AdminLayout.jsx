677128f6c0427fb67296e7176191ee33731bd29c
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-05 00:18:41 +0530

    commmit

--- src/Admin/AdminLayout.jsx
+++ src/Admin/AdminLayout.jsx
@@ -30,10 +30,10 @@ const AdminLayout = () => {
 
       {/* Sidebar */}
       <div className={`
-        fixed lg:static inset-y-0 left-0 z-40
+        fixed lg:static mt-28 inset-y-0 left-0 z-40
         transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}
         lg:translate-x-0 transition-transform duration-300 ease-in-out
-        w-64 bg-[#4CAF50] shadow-xl
+        w-64 bg-[#3CA65E] shadow-xl
       `}>
         <div className="flex flex-col h-full">
           {/* Logo Section */}

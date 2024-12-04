1b092d9955484baefda5e6f2651e04bdf61510ff
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-04 22:48:39 +0530

    Transporter

---
+++ Server/utls/transPorter.js
@@ -0,0 +1,13 @@
+// contactEmail.js
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_USER, // replace with your environment variable
+    pass: process.env.MAIL_PASS, // replace with your environment variable
+  },
+});
+
+module.exports = transporter;

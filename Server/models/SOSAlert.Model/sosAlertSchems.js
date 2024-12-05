c4e9c18d57c3ed8d1ea2fa1441c9bc27d7fbacd7
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-06 02:59:28 +0530

    sosalet.js

---
+++ Server/models/SOSAlert.Model/sosAlertSchems.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const sosAlertSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    busId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Bus",
+      required: true,
+    },
+    location: {
+      latitude: {
+        type: Number,
+        required: true,
+      },
+      longitude: {
+        type: Number,
+        required: true,
+      },
+    },
+    status: {
+      type: String,
+      enum: ["active", "resolved"],
+      default: "active",
+    },
+    description: {
+      type: String,
+    },
+    emergencyType: {
+      type: String,
+      enum: ["medical", "security", "accident", "other"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("SOSAlert", sosAlertSchema);

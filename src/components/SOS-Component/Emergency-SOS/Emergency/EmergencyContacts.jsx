88bb22798e3a9bc938ac8d747dc2505d157fe8ff
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-09 00:14:21 +0530

    commit

--- src/components/SOS-Component/Emergency-SOS/Emergency/EmergencyContacts.jsx
+++ src/components/SOS-Component/Emergency-SOS/Emergency/EmergencyContacts.jsx
@@ -62,6 +62,9 @@ function EmergencyContacts({ userId }) {
   };
 
   return (
+
+    // energency --
+    
     <div className="space-y-6">
       <div className="flex justify-between items-center mb-6">
         <h2 className="text-2xl font-bold text-gray-800">Emergency Contacts</h2>

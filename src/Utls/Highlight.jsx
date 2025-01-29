440325516ae9f520e0c9b28f833d5fc77f6b12d8
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-01-29 23:56:08 +0530

    highlight.jsx

--- src/Utls/Highlight.jsx
+++ src/Utls/Highlight.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 function Highlight({ text }) {
-  console.log(text);
-  // <span className="text-richblack-300 text-base font-bold w-[85%] -mt-3">
-  //   {subheading}
-  // </span>
+ 
   return (
     <div>
       <span className=" text-simon font-extrabold text-4xl">{text}</span>
